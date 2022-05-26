d3b56928d684bd3fe47bdf41ef495ab8726a3602
wickapp <wickhomie@gmail.com>
2022-05-26 18:11:04 +0300

    Initial Commit

---
+++ Shared/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  Shared
+//
+//  Created by Илья Терновской on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
